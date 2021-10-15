3e97c16c73c694311b5621fb3792d9ca1fe02fa7
JosiaAlmeida <josiaalmeida0001@gmail.com>
2021-10-15 18:29:13 +0100

    first commit

---
+++ Projeto1/classes/gravarficheiro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Projeto1.classes
+{
+    public class gravarficheiro
+    {
+        public void RecordFile()
+        {
+           StreamWriter ficheiro = new StreamWriter(@".\file.txt", true, Encoding.Default);
+        }
+    }
+}
